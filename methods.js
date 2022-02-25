bbde4992c88d76f2e085839b24e4a0712781d093
TukaMu <machadofelipe46@gmail.com>
2022-02-25 13:23:24 -0300

    V.2

--- methods.js
+++ methods.js
@@ -1,5 +1,6 @@
 const { text } = require('body-parser');
 const express = require('express');
+const { send } = require('express/lib/response');
 
 const methods = express.Router();
 
@@ -12,85 +13,66 @@ let db = [
 //
 
 
-//ENUNCIADO
-methods.get('/', (req, res) => {
-    return res.send('Listar = http://localhost:33533/listar\n Colocar = http://localhost:33533/colocar            +       json\n Deletar = http://localhost:33533/deletar:id                 id = id da tarefa\n Atualizar = http://localhost:33533/atualizar:id/:action     id = id da tarefa      action = 1 : iniciar tarefa\n                                                                                             || 2 : termina tarefa');
-})
-
 //MOSTRAR TAREFAS
 methods.get('/listar', (req, res) => {
-    return res.json(db);
+    return res.json(db)
 })
 
 //COLOCA TAREFAS
 methods.post('/colocar', (req, res) => {
-    const requerido = req.body;
+    const requerido = req.body
 
     if (!requerido)
-        return res.status(400).end();
-
+        return res.status(400).end()
     else if (db.find(item => item.id === requerido.id)) {
-        return res.send('Id já existente');
+        return res.send('Id já existente')
     }
     else {
-        db.push(requerido);
+        db.push(requerido)
+        return res.send(db)
     }
-    return res.send(db);
 })
 
 //DELETA TAREFA
 methods.delete('/deletar:id', (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
 
-    if (db.find(item => item.id === id)) {
-        let dbatt = db.filter(item => {
-            if (item.id != id)
-                return item;
-        })
-        return res.send(db = dbatt);
-    }
-    else {
-        return res.send('O Id não está cadastrado');
-    }
+    let dbatt = db.filter(item => {
+        if (item.id != id)
+            return item
+    })
+    return res.send(db = dbatt)
 
 })
 
 //ATUALIZAR TAREFA
-methods.put('/atualizar:id/:action', (req, res) => {
-    const id = req.params.id;
-    const action = req.params.action;
+methods.get('/tarefa:id/:action', (req, res) => {
+    const id = req.params.id
+    const action = req.params.action
+    const item = db.find(item => item.id === id)
 
-    if (db.find(item => item.id === id)) {
-        if (action == 1) {
-            let dbatt = db.filter(item => {
-                if (id == item.id) {
-                    item.tini = new Date();
-                }
-                return item;
-            })
-            return res.send(db = dbatt);
-        }
-        else if (action == 2) {
-            let dbatt = db.filter(item => {
-                if (id == item.id) {
-                    if (item.tini != '0') {
-                        item.tfim = new Date();
-                        item.ttotal = parseFloat(((item.tfim - item.tini) / 60000).toFixed(2)) + " minutos";
-                    }
-                    else {
-                        return res.send('A tarefa não foi iniciada!')
-                    }
-                }
-                return item;
-            })
-            return res.send(db = dbatt);
-        }
-        else {
-            return res.send('Ação incorreta!');
-        }
-
+    if (action == 1) {
+        item.tini = new Date()
     }
-    return res.send('Id incorreto!');
+    else if (item.tini != '0') {
+        item.tfim = new Date();
+        item.ttotal = parseFloat(((item.tfim - item.tini) / 60000).toFixed(2)) + " minutos"
+    }
+    return res.send(db)
+})
+
+//EDITAR TAREFA
+methods.put('/editar', (req, res) => {
+    const requerido = req.body;
+    if (requerido) {
+        const item = db.find(item => item.id === requerido.id)
+        item.Tarefa = requerido.Tarefa
+        item.Local = requerido.Local
+        return res.send(db)
+    }
+    else {
+        return res.status(400).end()
+    }
 })
 
 module.exports = methods
